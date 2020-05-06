77190bf98d20b264d1df9290a61ff4c7912ae8af
Arman Seisenbek <arman.seisenbek@gmail.com>
2020-05-06 21:51:05 +0600

    indentation fixes

--- index.php
+++ index.php
@@ -34,16 +34,14 @@
 
             <!-- Products catalog -->
             <?php 
-                $products = mysqli_query($connection, "SELECT * FROM `products`");
-            ?>
-
-            <?php while ( $prod = mysqli_fetch_assoc($products) ) { ?>
+            $products = mysqli_query($connection, "SELECT * FROM `products`");
+            while ( $prod = mysqli_fetch_assoc($products) ) { ?>
 
             <div class="col-lg-4 col-md-6 mb-4">
 
                 <a href="/pages/products.php?id=<?php echo $prod['id']; ?>">
                     <!-- background image of product card -->
-                    <div class="card" style="background-image: url(img/product-covers/<?php echo $prod['cover'] ?>);">
+                    <div class="card" style="background-image: url( img/product-covers/<?php echo $prod['cover'] ?> );">
                         <div class="card-img-overlay">
                             <!-- title of product card -->
                             <h4 class="card-title"><?php echo $prod['title']; ?></h4>  
