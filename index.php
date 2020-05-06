9173d476e06a7714f7a8270fcfaa8fee967bd53c
Arman Seisenbek <arman.seisenbek@gmail.com>
2020-05-06 22:01:59 +0600

    fixed burger bug

--- index.php
+++ index.php
@@ -65,6 +65,14 @@
     </div>
     <!-- / Page Content -->
 
+     <!-- jquery -->
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+    <!-- Bootstrap 4 js cdn -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+
     <!-- Footer -->
     <?php include "includes/footer.php"; ?>
     <!-- ./Footer -->
