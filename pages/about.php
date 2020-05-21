c72244c45266ace7b082dc9f0a20e791475d300a
Takhir <takhir.004@gmail.com>
2020-05-21 17:31:17 +0600

    added what Sanzhik asked to add

--- pages/about.php
+++ pages/about.php
@@ -9,7 +9,7 @@
 	<?php require "../includes/head.php"; ?>
 
 	<title>About us</title>
- 	
+
  	<!-- link to custom css -->
 	<link rel="stylesheet" type="text/css" href="../css/about_style.css">
 
@@ -29,8 +29,30 @@
             	Call us now to get more info: 8-747-452-08-73
             </p>
         </div>
-    </div>
+			</div>
 
+			<section class="counters">
+				<div class="container">
+					<div class="f-left">
+						<!-- Data target is numbers in that brackets -->
+						<div class="counter" data-target="17">3</div>
+						<h3>Уже купили</h3>
+
+					</div>
+					<div class="f-left">
+						<!-- Data target is numbers in that brackets -->
+						<div class="counter" data-target="17">3</div>
+						<h3>Партнеры</h3>
+
+					</div>
+					<div class="f-left">
+						<!-- Data target is numbers in that brackets -->
+						<div class="counter" data-target="17">3</div>
+						<h3>Сколько лет</h3>
+
+					</div>
+				</div>
+			</section>
 	<!-- section about our company -->
 	<section id="company" class="company">
 		<div class="container">
@@ -63,22 +85,22 @@
 					<p>Our business is built on good relations with our customers, so we will prepare your orders in the early morning</p>
 				</center>
 			</div>
-			
+
 			<div class="col-lg-4">
 				<center>
 					<!-- icon -->
 					<i class="fas fa-truck" style="font-size: 450%"></i>
-				
+
 					<h3>Delivery</h3>
 					<p>We guarantee you to deliver your order before noon, the products we made this morning.</p>
 				</center>
 			</div>
-		
+
 			<div class="col-lg-4">
 				<center>
 					<!-- icon -->
 					<i class="fas fa-briefcase" style="font-size: 450%"></i>
-				
+
 					<h3>Partnership</h3>
 					<p>We really value our customers and therefore often give various bonuses to our regular customers.</p>
 				</center>
@@ -89,15 +111,15 @@
 		<div class="row">
 
 			<div class="col-lg-3">
-				<img 
-					src="https://forbes.kz/img/articles/b05730715b012ac54a680349924c0d42-small.jpg" 
+				<img
+					src="https://forbes.kz/img/articles/b05730715b012ac54a680349924c0d42-small.jpg"
 					class="slide_img">
 			</div>
 			<div class="col-lg-9">
 				<h3>Location</h3>
 				<p>The farm is located at the foot of the Tien Shan Mountains, near the village of Shokpar in the Shuskiy district of the Zhambyl region of Kazakhstan. Our farm covers 25 hectares.</p>
 			</div>
-			
+
 		</div>
 
 		<div class="row">
@@ -135,7 +157,7 @@
 		</div>
 
 	</div>
-	
+
 	<!-- footer -->
 	<?php include "../includes/footer.php"; ?>
 
