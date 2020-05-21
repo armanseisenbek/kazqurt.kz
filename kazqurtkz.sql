45a9f3864ef91810b8234af72f672049026d311a
Arman Seisenbek <arman.seisenbek@gmail.com>
2020-05-21 22:41:51 +0600

    sql fixed

--- kazqurtkz.sql
+++ kazqurtkz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 21, 2020 at 07:04 PM
+-- Generation Time: May 21, 2020 at 07:41 PM
 -- Server version: 10.3.13-MariaDB
 -- PHP Version: 7.1.22
 
@@ -21,6 +21,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `kazqurtkz`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `customer_email` varchar(100) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -44,6 +57,21 @@ CREATE TABLE `customers` (
 
 INSERT INTO `customers` (`id`, `email`, `name`, `phone`, `city`, `review`, `password`) VALUES
 (3, 'arman.seisenbek@gmail.com', 'arman', '87077777272', 'Almaty', '', '$2y$10$1zgdP7BRfYTDD/RU5ru7Y.pZPJRUPfD.0IMNTaVlTqvWmXHE36usu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `customer_email` varchar(100) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -93,9 +121,21 @@ INSERT INTO `users` (`id`, `email`, `password`) VALUES
 --
 
 --
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -109,10 +149,22 @@ ALTER TABLE `products`
 --
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
